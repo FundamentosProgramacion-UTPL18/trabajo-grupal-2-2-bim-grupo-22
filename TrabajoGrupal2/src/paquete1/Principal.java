e5055cc83aec5a92645000ee8af003a7a1917478
Carlos Castillo <carloscastillo090916@gmail.com>
2018-07-05 11:59:05 -0500

    Trabajo grupal 2 Final

--- TrabajoGrupal2/src/paquete1/Principal.java
+++ TrabajoGrupal2/src/paquete1/Principal.java
@@ -3,11 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+ /*
+PROGRAMA QUE PERMITE CALCULAR EL PROMEDIO DE CALIFICACIONES.
+ */
 package paquete1;
 
 /**
  *
- * @author Carlos
+ * @authores Carlos Castillo & Renato Balcazar
  */
 public class Principal {
 
@@ -16,30 +19,35 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        String [ ] nombres = {"José", "María", "Ana", "Paul", "David"};
-        String [ ] apellidos = {"Yaguana", "Benitez", "Gaviria", "Davila", "Andrade"};
-        int [ ] edades = {20, 19, 20, 19, 25};
-        double [ ] calificaciones1 = {9.1, 10, 8, 7, 9.1};
-        double [ ] calificaciones2 = {6.1, 5, 10, 7.2, 4.1};
-        Estudiante [ ] estudiante = new Estudiante [ nombres.length] ;
-        
-        for (int contador = 0; contador < nombres.length; contador ++){
-            Estudiante est = new Estudiante();
+        //Arreglos globales inicializados.
+        String[] nombres = {"José", "María", "Ana", "Paul", "David"};
+        String[] apellidos = {"Yaguana", "Benitez", "Gaviria", "Davila", "Andrade"};
+        int[] edades = {20, 19, 20, 19, 25};
+        double[] calificaciones1 = {9.1, 10, 8, 7, 9.1};
+        double[] calificaciones2 = {6.1, 5, 10, 7.2, 4.1};
+        //Arreglo global que guarda los objetos.
+        Estudiante[] estudiante = new Estudiante[nombres.length];
+
+        //Ciclo que permite recorrer todos los arreglos.
+        for (int contador = 0; contador < nombres.length; contador++) {
+            Estudiante est = new Estudiante();  //Creamos el objeto de tipo Estudiante.
+
+            //Asiganamos los valores a cada objeto.
             est.nombres = nombres[contador];
             est.apellidos = apellidos[contador];
             est.edad = edades[contador];
             est.calificacion1 = calificaciones1[contador];
             est.calificacion2 = calificaciones2[contador];
-            estudiante[contador] = est;    
-            
+
+            estudiante[contador] = est;    //Llenamos el arreglo 'estudiante' con los objetos.
+
         }
-         for (int contador = 0; contador < estudiante.length; contador ++){
-             estudiante[contador].presentar();
-         }
-        
-        
-        
+
+        //ciclo que permite presentar el reporte de cada estudiante.
+        for (int contador = 0; contador < estudiante.length; contador++) {
+            estudiante[contador].presentar();
+        }
+
     }
-    
+
 }
